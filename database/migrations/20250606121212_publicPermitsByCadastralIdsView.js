c62933971d40c1fe070504e851ef3a9cf890345f
LWangllix <vanglikas.lukas@gmail.com>
2025-06-06 12:30:54 +0300

    create service

--- database/migrations/20250606121212_publicPermitsByCadastralIdsView.js
+++ database/migrations/20250606121212_publicPermitsByCadastralIdsView.js
@@ -1,37 +1,44 @@
 exports.up = function (knex) {
   return knex.raw(`
-  CREATE MATERIALIZED VIEW public_permits_by_cadastral_ids AS
-SELECT
-    plot ->> 'cadastral_number' AS cadastral_number,
-    json_agg(DISTINCT jsonb_build_object(
-        'permit_species_id', ps.id,
-        'species_classifier', to_jsonb(sc),
-        'family_classifiers', to_jsonb(fc)
-    )) AS permit_species,
-    json_agg(json_build_object(
-        'permit_number', p.permit_number,
-        'issued_to_user', json_build_object(
-            'name', u.first_name,
-            'lastname', u.last_name
-        ),
-        'issued_to_tenant', json_build_object(
-            'name', t.name
-        ),
-        'permit_id', p.id
-    )) AS permits
-FROM
-    permits p
-LEFT JOIN users u ON p.user_id = u.id
-LEFT JOIN tenants t ON p.tenant_id = t.id
-LEFT JOIN permit_species ps ON p.id = ps.permit_id
-LEFT JOIN species_classifiers sc ON ps.species_classifier_id = sc.id
-LEFT JOIN family_classifiers fc ON ps.family_classifier_id = fc.id
-JOIN LATERAL jsonb_array_elements(p.cadastral_ids) AS plot ON true
-GROUP BY
-    plot ->> 'cadastral_number';
+      CREATE MATERIALIZED VIEW public_permits_by_cadastral_ids AS
+      SELECT
+        plot ->> 'cadastral_number' AS cadastral_number,
+        json_agg(
+          json_build_object(
+            'permitNumber', p.permit_number,
+            'issuedToUser', json_build_object(
+              'firstName', u.first_name,
+              'lastName', u.last_name
+            ),
+            'issuedToTenant', json_build_object(
+              'name', t.name
+            ),
+            'id', p.id,
+            'permitSpecies', (
+              SELECT json_agg(
+                DISTINCT jsonb_build_object(
+                  'id', ps.id,
+                  'speciesClassifier', to_jsonb(sc),
+                  'familyClassifiers', to_jsonb(fc)
+                )
+              )
+              FROM permit_species ps
+              LEFT JOIN species_classifiers sc ON ps.species_classifier_id = sc.id
+              LEFT JOIN family_classifiers fc ON ps.family_classifier_id = fc.id
+              WHERE ps.permit_id = p.id
+            )
+          )
+        ) AS permits
+      FROM
+        permits p
+        LEFT JOIN users u ON p.user_id = u.id
+        LEFT JOIN tenants t ON p.tenant_id = t.id
+        JOIN LATERAL jsonb_array_elements(p.cadastral_ids) AS plot ON true
+      GROUP BY
+        plot ->> 'cadastral_number';
     `);
 };
 
 exports.down = function (knex) {
-  return knex.raw('DROP MATERIALIZED VIEW IF EXISTS public_permits_by_cadastral_ids');
+  return knex.raw('DROP MATERIALIZED VIEW IF EXISTS public_permits_by_cadastral_ids;');
 };
